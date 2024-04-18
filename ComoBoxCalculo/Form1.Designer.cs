80cb3b9c283967b507ec70c702c7f0e1e10a7f76
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-04-18 20:28:09 -0300

    Adicionar arquivos de projeto.

---
+++ ComoBoxCalculo/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ComoBoxCalculo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbAtividade = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericdropdownTempo = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericdropdownTempo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione o tipo de atividade fisica:";
+            // 
+            // cmbAtividade
+            // 
+            this.cmbAtividade.FormattingEnabled = true;
+            this.cmbAtividade.Items.AddRange(new object[] {
+            "Caminhada",
+            "Academia",
+            "Corrida",
+            "Natação",
+            "Bicicleta",
+            "Escalada"});
+            this.cmbAtividade.Location = new System.Drawing.Point(348, 104);
+            this.cmbAtividade.Name = "cmbAtividade";
+            this.cmbAtividade.Size = new System.Drawing.Size(314, 28);
+            this.cmbAtividade.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Informe seu PESO em KG:";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(291, 154);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(95, 26);
+            this.textBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Quanto tempo você irá fazer o exercicio:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "O Valor KCal Gasto é de:";
+            // 
+            // textBoxResultado
+            // 
+            this.textBoxResultado.Location = new System.Drawing.Point(283, 275);
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.Size = new System.Drawing.Size(100, 26);
+            this.textBoxResultado.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(110, 348);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(93, 36);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(431, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // numericdropdownTempo
+            // 
+            this.numericdropdownTempo.Location = new System.Drawing.Point(384, 215);
+            this.numericdropdownTempo.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericdropdownTempo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericdropdownTempo.Name = "numericdropdownTempo";
+            this.numericdropdownTempo.Size = new System.Drawing.Size(68, 26);
+            this.numericdropdownTempo.TabIndex = 11;
+            this.numericdropdownTempo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 670);
+            this.Controls.Add(this.numericdropdownTempo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.textBoxResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbAtividade);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericdropdownTempo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbAtividade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericdropdownTempo;
+    }
+}
+
---
+++ ComoBoxCalculo/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComoBoxCalculo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (cmbAtividade.SelectedIndex.Equals(-1))
+            {
+                MessageBox.Show("Insire uma opção válida de atividade física!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBoxResultado.Clear();
+                cmbAtividade.Focus();
+            }
+        }
+    }
+}
